2c10881530c178ba874b4343ce2f0ac08082ed82
vnm <victor.ameneses@outlook.es>
2024-11-01 18:27:13 +0400

    WithDockerFile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Use the official .NET SDK image to build the app
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build-env
+WORKDIR /app
+
+# Copy the project files and restore dependencies
+COPY *.csproj ./
+RUN dotnet restore
+
+# Copy the entire project and build it
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Use the runtime image for the final container
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build-env /app/out .
+
+# Expose the port your app runs on (default is 80)
+EXPOSE 80
+
+# Start the application
+ENTRYPOINT ["dotnet", "ApiCore.dll"]
--- obj/Debug/net8.0/ApiCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa32c3f6227933a5adfcaac5bc3fc95523afa261")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
